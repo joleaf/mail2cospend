8c05c3c15e5810dfbf02242abd29bd50cd6922f8
Jonas Blatt <jonasblatt@uni-koblenz.de>
2025-01-20 18:37:22 +0100

    Fix netto

--- src/mail2cospend/searchadapter/netto.py
+++ src/mail2cospend/searchadapter/netto.py
@@ -19,29 +19,25 @@ class NettoSearchAdapter(SearchAdapter):
 
     @classmethod
     def _use_plain_text_in_mail(cls) -> bool:
-        return False
+        return True
 
     @classmethod
     def _use_html_text_in_mail(self) -> bool:
-        return True
+        return False
 
     @property
     def _search_query(self) -> str:
-        return f'(FROM noreply@netto-app.de) (SUBJECT "Marken-Discount!") (SINCE "{self.config.get_since_for_imap_query()}")'
+        return f'(FROM nicht.antworten@reply.netto-online.de) (SUBJECT "Marken-Discount!") (SINCE "{self.config.get_since_for_imap_query()}")'
 
     def _get_bon_from_pdf(self, pdf: PdfReader, email_timestamp: datetime) -> Optional[BonSummary]:
         return None
 
     def _get_bon_from_text(self, payload: Iterable[str], email_timestamp: datetime, is_html: bool) -> Optional[
         BonSummary]:
-        found_summe = False
         sum = 0
         for row in payload:
-            if "Summe:" in row:
-                found_summe = True
-                continue
-            if found_summe:
-                sum = float(row.split("<b>")[1].split(" ")[0].replace(",", "."))
+            if "SUMME" in row:
+                sum = float(row.replace("SUMME","").replace(",",".").strip())
                 break
         bon = BonSummary(sum=sum, document="", timestamp=email_timestamp, adapter_name=self.adapter_name())
         return bon
